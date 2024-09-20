39c058b4d779b9951f5b5005637725f76977387a
Ammar-Nassr-ABF1190_ferg <ammar.nassri@ferguson.com>
2024-09-20 11:26:06 -0400

    updated resume

--- src/components/navigation/navigation.component.tsx
+++ src/components/navigation/navigation.component.tsx
@@ -1,59 +1,62 @@
 import { FunctionComponent } from "react";
 import { openInNewTab, scrollToElement } from "../../utils/general.utils";
-import resume from "../downloadable/Ammar_Nassri_Resume_2024.pdf";
 import { ClickableElement } from "../clickable-element/clickable-element.component";
+import resume from "../downloadable/ANassri_Resume_24.pdf";
 
 type NavigationButtonProps = {
-  text: string;
-  onClick: () => void;
-  isActive?: boolean;
+    text: string;
+    onClick: () => void;
+    isActive?: boolean;
 };
 
 const NavigationButton: FunctionComponent<NavigationButtonProps> = ({
-  text,
-  onClick,
-  isActive = false,
+    text,
+    onClick,
+    isActive = false,
 }) => {
-  return (
-    <ClickableElement
-      ariaLabel={text}
-      onClick={onClick}
-      className={`${
-        isActive
-          ? "text-regal-green"
-          : "text-gray hover:text-regal-green transition duration-250 ease-in-out"
-      }`}
-    >
-      {text}
-    </ClickableElement>
-  );
+    return (
+        <ClickableElement
+            ariaLabel={text}
+            onClick={onClick}
+            className={`${
+                isActive
+                    ? "text-regal-green"
+                    : "text-gray hover:text-regal-green transition duration-250 ease-in-out"
+            }`}
+        >
+            {text}
+        </ClickableElement>
+    );
 };
 
 type NavigationProps = {
-  sectionInView: "ABOUT" | "EXPERIENCE" | "PROJECTS";
+    sectionInView: "ABOUT" | "EXPERIENCE" | "PROJECTS";
 };
 
 export const Navigation: FunctionComponent<NavigationProps> = ({
-  sectionInView,
+    sectionInView,
 }) => {
-  return (
-    <div className="flex flex-col gap-y-4">
-      <NavigationButton
-        text="ABOUT"
-        onClick={() => scrollToElement("top")}
-        isActive={sectionInView === "ABOUT" ? true : false}
-      />
-      <NavigationButton
-        text="EXPERIENCE"
-        onClick={() => scrollToElement("experience")}
-        isActive={sectionInView === "EXPERIENCE" ? true : false}
-      />
-      <NavigationButton
-        text="PROJECTS"
-        onClick={() => scrollToElement("projects")}
-        isActive={sectionInView === "PROJECTS" ? true : false}
-      />
-      <NavigationButton text="RESUME" onClick={() => openInNewTab(resume)} />
-    </div>
-  );
+    return (
+        <div className="flex flex-col gap-y-4">
+            <NavigationButton
+                text="ABOUT"
+                onClick={() => scrollToElement("top")}
+                isActive={sectionInView === "ABOUT" ? true : false}
+            />
+            <NavigationButton
+                text="EXPERIENCE"
+                onClick={() => scrollToElement("experience")}
+                isActive={sectionInView === "EXPERIENCE" ? true : false}
+            />
+            <NavigationButton
+                text="PROJECTS"
+                onClick={() => scrollToElement("projects")}
+                isActive={sectionInView === "PROJECTS" ? true : false}
+            />
+            <NavigationButton
+                text="RESUME"
+                onClick={() => openInNewTab(resume)}
+            />
+        </div>
+    );
 };
