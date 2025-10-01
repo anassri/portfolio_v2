38ef8ae4f3708f8cbeb8135e499fd41781efcc8e
Ammar-Nassr-ABF1190_ferg <ammar.nassri@ferguson.com>
2025-10-01 14:53:35 -0400

    updated footer message

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -2,14 +2,14 @@ import { FunctionComponent } from "react";
 import { SocialMedia } from "../social-media/social-media.component";
 
 export const Footer: FunctionComponent = () => {
-  return (
-    <div className="flex flex-col items-center mx-2 lg:mx-0 lg:block">
-      <SocialMedia />
-      <p className="text-gray text-xs">
-        Designed in <b>Figma</b>. Built with <b>React</b> and
-        <b> Tailwind CSS</b>, deployed with <b>Vercel</b>. <br /> <br /> © Ammar
-        Nassri 2024
-      </p>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center mx-2 lg:mx-0 lg:block">
+            <SocialMedia />
+            <p className="text-gray text-xs">
+                Designed in <b>Figma</b>. Built with <b>React</b> and
+                <b> Tailwind CSS</b>, deployed with <b>Netlify</b>. <br />{" "}
+                <br /> © Ammar Nassri 2024
+            </p>
+        </div>
+    );
 };
